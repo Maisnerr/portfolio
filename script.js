5753127744c9ee8c81974fb9dddcf1ecd4dceda7
Maisnerr <maisner.podolsky@gmail.com>
2025-11-13 17:35:06 +0100

    Small design&readability changes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// Smoothly scrolls down to a section of given ID
 function scrollToSection(id) {
-    console.log(id);
-    console.log(typeof id);
     const el = document.getElementById(id);
     const offset = 78; 
     const top = el.getBoundingClientRect().top + window.pageYOffset - offset;
     window.scrollTo({ top, behavior: "smooth" });
 }
 
-window.addEventListener('scroll', () => {
+// Shows or hides the scroll-to-top button based on scroll position
+function showScrollButton() {
+  window.addEventListener('scroll', () => {
   const el = document.getElementById("back-top-button");
   if (window.scrollY != 0) {
     el.classList.add("show");
@@ -15,4 +16,21 @@ window.addEventListener('scroll', () => {
     el.classList.remove("show");
   }
 });
+}
 
+// Adjusts visibility of "See More" button based on device
+function seeMoreButtonVisibility() {
+  const el = document.getElementsByClassName("introduction-button")[0];
+  if (window.innerWidth <= 768) {
+    el.style.display = "none";
+  } else {
+    el.style.display = "block";
+  }
+}
+
+// Initialization function
+function onStart(){
+  showScrollButton(); // Start showing scroll button
+  seeMoreButtonVisibility(); // Set initial visibility of see more button
+}
+onStart(); // Call initialization function
