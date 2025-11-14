d72a25bb62d5707566296a9efcb3c9bb24513f3a
Maisnerr <maisner.podolsky@gmail.com>
2025-11-14 14:57:35 +0100

    Start.bat added

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+start "" "https://chatgpt.com/"
+
+start "" "file:///C:/Users/freen/OneDrive/Plocha/lukas_zaloha/projekty/dulezite/PORTFOLIO/index.html"
+
+start "" "file:///C:/Users/freen/OneDrive/Plocha/lukas_zaloha/projekty/dulezite/PORTFOLIO/"
+
+start "" "code"
+
+exit
+exit
