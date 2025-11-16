0a5f16327e2c8d123c5e7c96e4d37c38c51d075b
Maisnerr <maisner.podolsky@gmail.com>
2025-11-16 13:11:41 +0100

    Started on footer

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const SECRET_NAVBAR = document.getElementById("secret-navbar");
 let Secret_navbar_visible = false;
 
-function switch_secret_navbar(){
+// Enable(disable) Navbar menu on mobile 
+function switch_secret_navbar(){ 
     Secret_navbar_visible = !Secret_navbar_visible;
     console.log(Secret_navbar_visible);
 
@@ -14,4 +15,12 @@ function switch_secret_navbar(){
         SECRET_NAVBAR.style.opacity = "0";
         SECRET_NAVBAR.style.visibility = "hidden";
     }
+}
+
+// Smoothly scrolls down to a section of given ID
+function scrollToSection(id) {
+    const el = document.getElementById(id);
+    const offset = 0; 
+    const top = el.getBoundingClientRect().top + window.pageYOffset - offset;
+    window.scrollTo({ top, behavior: "smooth" });
 }
