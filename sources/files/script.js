2a1aab3e18ce5c702452d1be8aa108c96c0d30ad
Maisnerr <maisner.podolsky@gmail.com>
2025-11-17 22:36:48 +0100

    Progress on mobile Projects tab

--- sources/files/script.js
+++ sources/files/script.js
@@ -1,6 +1,27 @@
 const SECRET_NAVBAR = document.getElementById("secret-navbar");
 let Secret_navbar_visible = false;
 
+const projects_digits = document.getElementById("project-digit");
+let selected_number = 1;
+const projects = {
+    1: {
+        head: "abc",
+        body: "123"
+    },
+    2: {
+        head: "bcd",
+        body: "234"
+    },
+    3: {
+        head: "cde",
+        body: "345"
+    },
+    4: {
+        head: "def",
+        body: "456"
+    }
+};
+
 // Enable(disable) Navbar menu on mobile 
 function switch_secret_navbar(){ 
     Secret_navbar_visible = !Secret_navbar_visible;
@@ -11,16 +32,35 @@ function switch_secret_navbar(){
         SECRET_NAVBAR.style.opacity = "1";
         SECRET_NAVBAR.style.visibility = "visible";
     }else{
-        SECRET_NAVBAR.classList.remove("aos-animate")
+        SECRET_NAVBAR.classList.remove("aos-animate");
         SECRET_NAVBAR.style.opacity = "0";
         SECRET_NAVBAR.style.visibility = "hidden";
     }
 }
 
-// Smoothly scrolls down to a section of given ID
-function scrollToSection(id) {
-    const el = document.getElementById(id);
-    const offset = 0; 
-    const top = el.getBoundingClientRect().top + window.pageYOffset - offset;
-    window.scrollTo({ top, behavior: "smooth" });
+function test(){
+    console.log(selected_number);
+    console.log(projects[selected_number].head);
+    console.log(projects[selected_number].body);
+
+    projects_digits.children[selected_number-1].classList.add("project-digit-selected");
 }
+function test1(number){
+    projects_digits.children[selected_number-1].classList.remove("project-digit-selected");
+    
+    selected_number = number;
+    test();
+}
+function test2(number){
+    projects_digits.children[selected_number-1].classList.remove("project-digit-selected");
+
+    if(selected_number+Number(number)==5){
+        selected_number = 0;
+    } else if(selected_number+Number(number)==0){
+        selected_number = 5;
+    }
+
+    selected_number += Number(number);
+    test();
+}
+console.log(selected_number);
